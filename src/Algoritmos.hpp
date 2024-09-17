b329468165890ca9a62f0bc58d282ce8d47d0d50
FelipeMena11 <felipe.menarodriguez@ucr.ac.cr>
2024-09-17 08:20:15 -0600

    Merged main and greedy branches

--- src/Algoritmos.hpp
+++ src/Algoritmos.hpp
@@ -2,26 +2,35 @@
 #define ALGORITMOS_HPP
 
 #include <bits/stdc++.h>
+#include <algorithm>
+#include <random>
+
+#include "Compare.hpp"
+
 using namespace std;
 
 class Algoritmos {
  private:
+  // Contador de tiempo:
   double timeElapsed = 0;
+  // Matriz/Tablero objetivo:
+  const vector<int> objetivo = {1, 2, 3, 4, 5, 6, 7, 8, 0};
+  // Tablero de juego:
   vector<int> tablero = {3, 2, 1, 4, 8, 6, 7, 5, 0};
-  vector<vector<int>> posiblesMovimientos(const vector<int>&);
 
+  vector<vector<int>> posiblesMovimientos(const vector<int>&);
  public:
   Algoritmos();
   ~Algoritmos();
-
   void shuffle();
-
+  void printBoard();
+  
   // Auxiliares
   int iterativeDeepeningDfs(vector<int> estado, set<vector<int>> &visitados, int depth);
 
   // Algoritmos
   bool anchoPrimero();
-  void greedy();
+  bool greedy();
   void solucionadorIDS();
   void IDSHeuristica();
 
--- src/Menu.hpp
+++ src/Menu.hpp
@@ -1,30 +1,40 @@
 #ifndef MENU_HPP
 #define MENU_HPP
 
+#include "Algoritmos.hpp"
+
+#include <cmath>
+#include <ctime>
+#include <sched.h>
+#include <time.h>
+#include <cstdint>
+#include <ostream>
+#include <unistd.h>
 #include <cstdint>
 #include <iostream>
 #include <map>
 #include <string>
 
-#define OPCIONES_PRINCIPAL                                                        \
-  "╔═══════════════════════╗\n" \
-  "║    Menú Principal     ║\n"                                              \
-  "║ 1. Ancho Primero      ║\n"                                               \
-  "║ 2. Greedy             ║\n"                                              \
-  "║ 3. Solucionador IDS   ║\n"                                              \
-  "║ 4. IDS Heuristica     ║\n"                                              \
-  "║ 5. Créditos           ║\n"                                              \
-  "║ 6. Salir              ║\n"                                               \
-  "╚═══════════════════════╝\n" \
+#define OPCIONES_PRINCIPAL     \
+  "╔═══════════════════════╗\n"\
+  "║    Menú Principal     ║\n"\
+  "║ 1. Ancho Primero      ║\n"\
+  "║ 2. Greedy             ║\n"\
+  "║ 3. Solucionador IDS   ║\n"\
+  "║ 4. IDS Heuristica     ║\n"\
+  "║ 5. Generar tablero    ║\n"\
+  "║ 6. Créditos           ║\n"\
+  "║ 7. Salir              ║\n"\
+  "╚═══════════════════════╝\n"\
   "Seleccione una opción: "
 
 
-#define CREDITOS                                                                                    \
-  "╔═════════════════════════════╗\n" \
-  "║          Créditos           ║\n"                                                          \
-  "║ Christopher Acosta Madrigal ║\n"                                                           \
-  "║ Alberto González Avendaño   ║\n"                                                           \
-  "║ Christopher Acosta Madrigal ║\n"                                                           \
+#define CREDITOS                     \
+  "╔═════════════════════════════╗\n"\
+  "║          Créditos           ║\n"\
+  "║ Christopher Acosta Madrigal ║\n"\
+  "║ Alberto González Avendaño   ║\n"\
+  "║ Felipe Mena Rodríguez       ║\n"\
   "╚═════════════════════════════╝\n"
 
 class Menu {
