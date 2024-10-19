6f355a80677b135f44f8dc204f598ad51c7c620d
Alberto González Avendaño <albertoga03@gmail.com>
2024-10-18 22:31:35 -0600

    HillClimbing para resolver N Reinas

--- Proyecto1/src/HillClimbing.py
+++ Proyecto1/src/HillClimbing.py
@@ -1,4 +1,51 @@
 import Distribucion as dist
 import NReinas as reinas
 import Viajero as viajero
+import random
 
+# Hill Climbing para resolver el problema de las n reinas
+def hill_climbing_n_reinas(tablero):
+    n = len(tablero)
+    costo = reinas.conflictos(tablero)
+    
+    print("Tablero inicial:")
+    reinas.imprimir_tablero(tablero)
+
+    while True:
+        movimientos = reinas.movimientos(tablero)
+
+        mejor_movimiento = []
+        costo_movimiento = n*n
+        # Buscar el mejor movimiento
+        for movimiento in movimientos:
+            nuevo_costo = reinas.conflictos(movimiento)
+            if (nuevo_costo < costo_movimiento):
+                mejor_movimiento = movimiento
+                costo_movimiento = nuevo_costo
+
+        # Si el mejor movimiento es mejor que la solucion actual, se actualiza
+        if costo_movimiento < costo:
+            tablero, costo = mejor_movimiento, costo_movimiento
+        else:
+            break
+    
+    return tablero, costo
+
+# Hill Climbing para resolver el problema de las n reinas
+def hill_climbing_viajero():
+    pass
+
+# Hill Climbing para resolver el problema de las n reinas
+def hill_climbing_n_distribucion():
+    pass
+
+def n_reinas(n):
+    # Tablero inicial aleatorio
+    tablero = list(range(n))
+    random.shuffle(tablero)
+    # Hill Climbing
+    solucion, conflictos = hill_climbing_n_reinas(tablero)
+    print("\nSolución final:")
+    reinas.imprimir_tablero(solucion)
+
+n_reinas(8)
--- Proyecto1/src/NReinas.py
+++ Proyecto1/src/NReinas.py
@@ -1,1 +1,33 @@
 # Funciones comunes para resolver el problema de las N reinas
+
+# Calcula el número de conflictos entre las reinas en el tablero
+def conflictos(tablero):
+    conflictos = 0
+    n = len(tablero)
+    for i in range(n):
+        for j in range(i+1, n):
+            # Si estan en la misma fila o en la misma diagonal
+            if (tablero[i] == tablero[j]) or (abs(tablero[i]-tablero[j]) == abs(i-j)):
+                conflictos += 1
+    return conflictos
+
+# Retorna una lista con los posibles movimientos (tableros que resultan de 
+# intercambiar dos reinas)
+def movimientos(tablero):
+    movimientos = []
+    n = len(tablero)
+    for i in range(n):
+        for j in range(i+1, n):
+            vecino = tablero[:]
+            vecino[i], vecino[j] = vecino[j], vecino[i]
+            movimientos.append(vecino)
+    return movimientos
+
+# Imprimir el tablero 
+def imprimir_tablero(tablero):
+    print(f"Numero de conflictos: {conflictos(tablero)}")
+    n = len(tablero)
+    for i in range(n):
+        fila = ['.'] * n
+        fila[tablero[i]] = 'R'
+        print(' '.join(fila))
