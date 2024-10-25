001ace165ddde953daa144707b64af544d0fb053
Sebastián Sandi <sebastiansandi@Sebastians-MacBook-Pro-3.local>
2024-10-24 22:04:31 -0600

    Problema de distribucion con hill climbing

--- Proyecto1/src/HillClimbing.py
+++ Proyecto1/src/HillClimbing.py
@@ -31,13 +31,34 @@ def hill_climbing_n_reinas(tablero):
     
     return tablero, costo
 
-# Hill Climbing para resolver el problema de las n reinas
+# Hill Climbing para resolver el problema del viajero
 def hill_climbing_viajero():
     pass
 
-# Hill Climbing para resolver el problema de las n reinas
-def hill_climbing_n_distribucion():
-    pass
+# Hill Climbing para resolver el problema de distribución
+def hill_climbing_n_distribucion(tiempos_maquina_1, tiempos_maquina_2):
+    asignacion_actual = dist.primera_asignacion(tiempos_maquina_1, tiempos_maquina_2)
+    tiempo_actual = dist.calcular_tiempos(asignacion_actual, tiempos_maquina_1, tiempos_maquina_2)
+
+    while True:
+        cambios = dist.cambios_asignacion(asignacion_actual)
+        
+        tiempos = []
+        for vecino in cambios:
+            nuevo_tiempo = dist.calcular_tiempos(vecino, tiempos_maquina_1, tiempos_maquina_2)
+            tiempos.append(nuevo_tiempo)
+
+        mejor_tiempo = min(tiempos)
+        mejor_asignacion = cambios[tiempos.index(mejor_tiempo)]
+
+        if mejor_tiempo < tiempo_actual:
+            tiempo_actual = mejor_tiempo
+            asignacion_actual = mejor_asignacion
+        else:
+            break
+    
+    return tiempo_actual, asignacion_actual
+
 
 def n_reinas(n):
     # Tablero inicial aleatorio
@@ -48,4 +69,13 @@ def n_reinas(n):
     print("\nSolución final:")
     reinas.imprimir_tablero(solucion)
 
+def n_distribucion(tiempos_maquina_1, tiempos_maquina_2):
+    tiempo, asignacion = hill_climbing_n_distribucion(tiempos_maquina_1, tiempos_maquina_2)
+    print("\nSolución final:")
+    dist.imprimir(asignacion, tiempos_maquina_1, tiempos_maquina_2)
+
+tiempos_maquina_1 = [2, 6, 3, 4, 2]
+tiempos_maquina_2 = [7, 2, 5, 3, 6]
+
+n_distribucion(tiempos_maquina_1, tiempos_maquina_2)
 n_reinas(8)
